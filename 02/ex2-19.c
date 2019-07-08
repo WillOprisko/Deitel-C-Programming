0ea5925f2c0c2d50fb2e32e88f6e3fa4d6f5fae6
Will Oprisko <coscmpsci@gmail.com>
2019-07-08 10:30:27 -0400

    Updated Ex2-19

--- 02/ex2-19.c
+++ 02/ex2-19.c
@@ -21,28 +21,28 @@ int main(void)
     printf("Third number:  ");
     scanf("%d", &n3);
     
-    printf("Sum is      %d\n", n1 + n2 + n3);
-    printf("Average is  %d\n", (n1 + n2 + n3) /3 );
-    printf("Product is  %d\n", n1 * n2 * n3);
+    printf("Sum         %d\n", n1 + n2 + n3);
+    printf("Average     %d\n", (n1 + n2 + n3) /3 );
+    printf("Product     %d\n", n1 * n2 * n3);
     
     if (n1 < n2 && n1 < n3)
-        printf("Smallest is %d\n", n1);
+        printf("Smallest    %d\n", n1);
     
     if (n2 < n1 && n2 < n3)
-        printf("Smallest is %d\n", n2);
+        printf("Smallest    %d\n", n2);
     
     if (n3 < n1 && n3 < n2)
-        printf("Smallest is %d\n", n3);
+        printf("Smallest    %d\n", n3);
     
     
     if (n1 > n2 && n1 > n3)
-        printf("Largest is  %d\n", n1);
+        printf("Largest     %d\n", n1);
     
     if (n2 > n1 && n2 > n3)
-        printf("Largest is  %d\n", n2);
+        printf("Largest     %d\n", n2);
     
     if (n3 > n1 && n3 > n2)
-        printf("Largest is  %d\n", n3);
+        printf("Largest     %d\n", n3);
     
     return 0;
 }
