7186604a9f4b4515f047704e6121e0d7852d1585
Will Oprisko <coscmpsci@gmail.com>
2019-07-08 11:22:06 -0400

    Completed Ex 2-20

--- 02/ex2-20.c
+++ 02/ex2-20.c
@@ -8,6 +8,15 @@
 
 int main(void)
 {
-   
-   return 0;
+  int   r    = 0;
+  float pi   = 3.14159;
+
+  printf("%s", "Please enter a radius: ");
+  scanf("%d", &r);
+
+  printf("%s\t%d\n", "Diameter", r * 2);
+  printf("%s\t%.2f\n", "Circumference", (2 * pi) * r);
+  printf("%s\t\t%.2f\n", "Area", pi * (r*r));
+  
+  return 0;
 }
