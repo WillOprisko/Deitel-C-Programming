fbc0189f7d9660c24f6eb6ca6463dc032463295c
Will Oprisko <coscmpsci@gmail.com>
2019-07-08 10:15:12 -0400

    Initialize Deitel C Programming Repo

---
+++ 02/ex2-13.c
@@ -0,0 +1,33 @@
+/*****************************
+ * Deitel - C Programming
+ * Chapter 2.16
+ * Arithmetic Operators
+ * "obtain two numbers from the user and print the them fromds the user and prints
+ *  sum, product, difference, quotient and remainder"
+ ******************************/
+
+#include <stdio.h>
+
+
+//  Function main() begins program execution  //
+int main(void)
+{
+   int num1 = 0;     // variable for the first  number entered by the user
+   int num2 = 0;     // variable for the second number entered by the user
+   
+   printf("Please enter two integers.\n");
+   
+   printf("first number: \n");
+   scanf("%d", &num1);                 // obtain and assign the first integer
+   
+   printf("second number: \n");
+   scanf("%d", &num2);                 // obtain and assign the second integer
+   
+   printf("%d + %d = %d\n", num1, num2, num1 + num2);
+   printf("%d * %d = %d\n", num1, num2, num1 * num2);
+   printf("%d - %d = %d\n", num1, num2, num1 - num2);
+   printf("%d / %d = %d\n", num1, num2, num1 / num2);
+   printf("%d % %d = %d\n", num1, num2, num1 % num2);
+   
+   return 0;
+}  // End function main() //
---
+++ 02/ex2-3.c
@@ -0,0 +1,27 @@
+/*****************************
+* Deitel - C Programming
+* Chapter 2.3
+* "Adding Two Integers"
+*
+******************************/
+#include <stdio.h>
+
+// Function main() begins program execution //
+int main(void)
+{
+   int n1  = 0;      // variable to contain the first number entered by the user
+   int n2  = 0;      // variable to contain the second number entered by the user
+   int sum = 0;      // variable to contain the sum of the n1 and n2
+   
+   printf("Enter the first integer\n");      // prompt the user to enter number 1
+   scanf("%d", &n1);                         // stores the value in n1
+   
+   printf("Enter the second integer\n");     // prompt the user to enter number 2
+   scanf("%d", &n2);                         // stores the value in n2
+   
+   sum = n1 + n2;    // assigns the value of the sum of n1 and n2
+   
+   printf("The sum is %d\n", sum);           // prints the value stored in sum
+   
+   return 0;
+}  // End function main() //
