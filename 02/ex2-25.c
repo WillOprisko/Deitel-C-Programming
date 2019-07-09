44a1c7afe4e95b8edeb52abe0f0303eaaf1a6710
Will Oprisko <coscmpsci@gmail.com>
2019-07-08 22:37:52 -0400

    In-progres -- ex2-25

---
+++ 02/ex2-25.c
@@ -0,0 +1,35 @@
+/*****************************
+ * Deitel - C Programming
+ * Chapter 2.25
+ * "Print Initials"
+ ******************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+   
+   return 0;
+}
+   GGG
+  G   G
+ G
+ G   GGG
+  G   G
+   GGG
+   
+   
+  W       W
+  W   W   W
+  W  W W  W
+   W W  W W
+    W    W
+
+   
+   OOOO
+  O    O
+ O      O
+ O      O
+ O      O
+  O    O
+   OOOO
