c02abca554b2e9e20b22118a45a8d32247b4f4f6
Will Oprisko <coscmpsci@gmail.com>
2019-07-09 09:13:21 -0400

    Refactored if-else structure

--- 02/ex2-26.c
+++ 02/ex2-26.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-   unsigned int n1 = 0;
-   unsigned int n2 = 0;
+   int n1 = 0;
+   int n2 = 0;
    
    puts("Welcome to the Multiples Program!");
-   puts("Please enter two non-negative integers.");
+   puts("Please enter two integers.");
    
    printf("%s", "First:  ");
    scanf("%d", &n1);
@@ -20,14 +20,20 @@ int main(void)
    printf("%s", "Second: ");
    scanf("%d", &n2);
    
+   
    if ((n2 % n1) == 0)
    {
       printf("The number %d is a multiple of %d\n", n1, n2);
    }
-   else
+   else if ((n2 % n1) != 0)
    {
       printf("The number %d is not a multiple of %d\n", n1, n2);
    }
+   else
+   {
+      puts("You did not enter a valid non-negative integer.");
+   }
+   
    
    return 0;
 }
