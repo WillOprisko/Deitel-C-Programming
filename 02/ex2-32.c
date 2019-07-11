7dfeb61fb074627b67a721a6fe57f9d60c1d5d5b
Will Oprisko <coscmpsci@gmail.com>
2019-07-11 11:26:10 -0400

    Control-Flow for user choice

--- 02/ex2-32.c
+++ 02/ex2-32.c
@@ -24,13 +24,32 @@
 
 int main(void)
 {
-   int choice = 0;
+   int  choice = 0;
+   float index = 0;
    
    puts("Welcome to the Body Mass Index Calculator!");
    puts("Please make a selection.");
    puts("1) U.S. customary Units");
    puts("2) International Metric Units");
    puts("3) Exit program");
+   
+   printf("%s", ">>  ");
+   scanf("%d", &choice);
+   
+   
+   // Control Flow for User Input //
+   // Exit program if the user enters '3' or an invalid value
+   if (choice != 1 || choice != 2)
+   {
+      puts("It appears you would like to exit the program.");
+      puts("Have a nice day!");
+      return 0;
+   }
+   //
+   else if (choice == 1 || choice == 2)
+   {
+      
+   }
    
    
    return 0;
