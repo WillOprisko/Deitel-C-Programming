26211d49861dd0b1872d0dd2bd591a315b7e3ee0
Will Oprisko <coscmpsci@gmail.com>
2019-07-11 11:20:12 -0400

    Outlined Program Structure

--- 02/ex2-32.c
+++ 02/ex2-32.c
@@ -3,3 +3,40 @@
  * Chapter 2.32
  * "Body Mass Index Calculator"
  ******************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ I.   main()
+ II.  calculateBMI()
+      A. Receive int       // user input
+      B. Return  float     // calculated BMI
+ III. customaryUnits()
+      A. Receive void
+      B. Return float      // US measurements BMI
+ IV.  metricUnits()
+      A. Receive void
+      B. Return float      // Metric units BMI
+ V.   displayBMI()
+*/
+
+
+int main(void)
+{
+   int choice = 0;
+   
+   puts("Welcome to the Body Mass Index Calculator!");
+   puts("Please make a selection.");
+   puts("1) U.S. customary Units");
+   puts("2) International Metric Units");
+   puts("3) Exit program");
+   
+   
+   return 0;
+}
+
+
+
+
+
