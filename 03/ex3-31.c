63fcd2ac6f87ff331a419cf244f6865a38ed0044
Will Oprisko <coscmpsci@gmail.com>
2019-08-14 15:07:59 -0400

    Exercise 3.31

---
+++ 03/ex3-31.c
@@ -0,0 +1,34 @@
+/*************************************************
+ * Deitel - C Programming
+ * Chapter 3 -- Exercise 3.31
+ * "Another Dangling Else Problem"
+ *************************************************/
+
+/*
+ -------------------------------------------------------------------------------
+ ::  Program Description  ::
+ -------------------------------------------------------------------------------
+ Modify the following code to produce the output shown. Use proper indentation
+ techniques. You may not make any changes other than inserting braces:
+ 
+ if ( y == 8 )
+ if ( x == 5 ) puts( "@@@@@" ); else
+ puts( "#####" ); puts( "$$$$$" ); puts( "&&&&&" );
+ 
+ */
+
+
+#include <stdio.h>
+
+void danglingElseA (int, int);
+void danglingElseB (int, int);
+
+int main(void)
+{
+
+   
+     
+   
+   
+   return 0;
+}
