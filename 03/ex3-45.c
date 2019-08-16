bba2f5e35fc537916a84372dc1f689ae96855cf3
Will Oprisko <coscmpsci@gmail.com>
2019-08-16 13:43:45 -0400

    Program Description Written

---
+++ 03/ex3-45.c
@@ -0,0 +1,24 @@
+/*************************************************
+ * Deitel - C Programming
+ * Chapter 3 -- Exercise 3.45
+ * "Factorial"
+ *************************************************/
+
+/*
+ ----------------------------------------------------------------------
+ ::  Program Description  ::
+ ----------------------------------------------------------------------
+ The factorial of a nonnegative integer n is written n!
+ (pronounced “n factorial”) and is defined as follows:
+ 
+      n!=n·(n-1)·(n-2)·...·1 (for values of n greater than or equal to 1)
+ and
+      n!=1 (for n = 0)
+ 
+ Example: 5! = 5 · 4 · 3 · 2 · 1, which is 120.
+      a) Write a program that reads a nonnegative integer and computes and prints its factorial.
+ 
+      b) Write a program that estimates the value of the mathematical constant e by using the formula:
+ 
+      c) Write a program that computes the value of (e)x by using the formula
+ */
