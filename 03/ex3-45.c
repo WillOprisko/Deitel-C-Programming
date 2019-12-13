eb83a27eaa4c359600713f49763603ebbfeb07ab
Will Oprisko <coscmpsci@gmail.com>
2019-12-13 11:49:11 -0500

    Exercise 3.45 Completed

--- 03/ex3-45.c
+++ 03/ex3-45.c
@@ -25,26 +25,30 @@
 
 #include <stdio.h>
 
-unsigned long long factorialGenerator (int n);
-long double eConstantGenerator (int n, int x);
-//long double eXDerivativeGenerator (int n);
+unsigned long long factorialGenerator  (int n);
+unsigned long long exponentGenerator   (int n, int x);
+long double eConstantGenerator         (int n, int x);
+long double eDerivativeGenerator       (int n);
 
 int main(void)
 {
    int userInput = 0;
-   unsigned long long factorial = 1;
-   long double eConstant = 1;
+   unsigned long long factorial  = 1;
+   long double eConstant         = 1;
+   long double eDerivative       = 1;
    
    puts  ("Welcome to the Factorial Generator!");
    puts  ("Provide an integer between 0-65.");
    printf("Number: ");
    scanf ("%d", &userInput);
    
-   factorial = factorialGenerator(userInput);
-   eConstant = eConstantGenerator(userInput, 1);
+   factorial   = factorialGenerator(userInput);
+   eConstant   = eConstantGenerator(userInput, 1);
+   eDerivative = eDerivativeGenerator(userInput);
    
    printf("%d!: %llu\n", userInput, factorial);
    printf("e:  %Lf\n", eConstant);
+   printf("e^x: %Lf\n", eDerivative);
    
    return 0;
 }
@@ -77,4 +81,39 @@ long double eConstantGenerator (int n, int x)
    }
    
    return eConstant;
+}
+
+
+long double eDerivativeGenerator (int n)
+{
+   unsigned long long factorial = 1;
+   unsigned long long exponent  = 1;
+   long double eDerivative = 1;
+   
+   for (int i=1; i <= n; i++)
+   {
+      exponent = exponentGenerator(n, i);
+      printf("%llu\n", exponent);
+      
+      factorial = factorialGenerator(i);
+      printf("%llu\n", factorial);
+      
+      eDerivative += (long double) exponent / (long double) factorial;
+      printf("%Lf\n", eDerivative);
+   }
+   
+   return eDerivative;
+}
+
+
+unsigned long long exponentGenerator   (int n, int x)
+{
+   unsigned long long exponent = 1;
+   for (int i = 1; i <= x; i++)
+   {
+      exponent *= n;
+      //printf("%llu\n", exponent);
+   }
+   
+   return exponent;
 }
